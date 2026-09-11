ed858b5217a2dfddd087c0732fa8874bb9ded4cc
maelosorio <ismaelosorio78@gmail.com>
2026-09-10 21:51:24 -0300

    Configuração inicial do projeto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-800">
+        Dose de Cuidado 💊
+      </h1>
+    </div>
+  )
+}
+
+export default App
